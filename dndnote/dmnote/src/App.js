c125c46168b3ebb5d02e015c97daf86fead92a68
ulysses-io <grant.swalwell-1@ou.edu>
2020-02-13 22:47:14 -0600

    second commit, timeline basics and map basics

--- dndnote/dmnote/src/App.js
+++ dndnote/dmnote/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import './react-tabs.css';
 import  Timeline  from './Timeline.js';
-import Map from "./Map.js";
+import Map from "./MapPanel.js";
 //import Timeline from "./Timeline";
 //import AddTLI from "./Timeline";
 
@@ -17,31 +16,32 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-
       <Tabs>
         <TabList>
           <Tab>Title 1</Tab>
           <Tab>Title 2</Tab>
         </TabList>
+        
+        <TabPanel>
+          <div>
+            <Map />
+          </div> 
+        </TabPanel>
 
         <TabPanel>
           <div>
             <Timeline/>
           </div>
         </TabPanel>
-
-        <TabPanel>
-          
-          <div>
-            <Map />
-          </div> 
-          
-        </TabPanel>
-
-
+        
+        
+      
       </Tabs>
-
       </header>
+
+      <body className="App-body">
+      
+      </body>
     </div>
   );
 }
--- dndnote/dmnote/src/Timeline.js
+++ dndnote/dmnote/src/Timeline.js
@@ -2,35 +2,32 @@ import React from 'react';
 
 import { Timeline, TimelineItem }  from 'vertical-timeline-component-for-react';
 
+var innerDateStyle = { background: '#61b8ff', color: '#000' };
+
+var bodyContStyle = { background: '#ddd', padding: '20px', borderRadius: '8px', 
+                        boxShadow: '0.5rem 0.5rem 2rem 0 rgba(0, 0, 0, 0.2)'};
 
 
-var elements = [    <TimelineItem 
-  key="001" 
-  dateText="11/2010 – Present" 
-  dateInnerStyle={{ background: '#61b8ff', color: '#000' }}
-  bodyContainerStyle={{
-    background: '#ddd',
-    padding: '20px',
-    borderRadius: '8px',
-    boxShadow: '0.5rem 0.5rem 2rem 0 rgba(0, 0, 0, 0.2)',
-  }}>
-    <h>YO</h>
-    <p>booooiii</p>
-</TimelineItem>, 
+function element(_key, head, body) {
+  return (
+    <TimelineItem 
+      key={_key}
+      dateText="11/2010 – Present" 
+      dateInnerStyle={innerDateStyle}
+      bodyContainerStyle={bodyContStyle}>
+        <h>{head}</h>
+        <p>{body}</p>
+    </TimelineItem>
+  )
+}
 
-<TimelineItem 
-key="002" 
-dateText="11/2010 – Present" 
-dateInnerStyle={{ background: '#61b8ff', color: '#000' }}
-bodyContainerStyle={{
-  background: '#ddd',
-  padding: '20px',
-  borderRadius: '8px',
-  boxShadow: '0.5rem 0.5rem 2rem 0 rgba(0, 0, 0, 0.2)',
-}}>
-  <h>YO</h>
-  <p>booooiii</p>
-</TimelineItem> ]
+
+
+var elements = [   
+  element("001", "head 0", "body body body 0"), 
+  element("002", "head 1", "body body body 1"),
+  element("002", "head 2", "body body body 2")
+ ]
 
 
 
@@ -52,24 +49,11 @@ class TimelineWithButton extends React.Component {
   handleClick(event) {
     event.preventDefault();
     elements.push(
-      <TimelineItem 
-        key="004" 
-        dateText="11/2010 – Present" 
-        dateInnerStyle={{ background: '#61b8ff', color: '#000' }}
-        bodyContainerStyle={{
-          background: '#ddd',
-          padding: '20px',
-          borderRadius: '8px',
-          boxShadow: '0.5rem 0.5rem 2rem 0 rgba(0, 0, 0, 0.2)',
-        }}>
-          <h>YO</h>
-          <p>{this.state.comment}</p>
-      </TimelineItem>);
-
-    
-    this.setState((state) => {
-      return {counter : state.counter + 1, comment: ''};
-    });
+      element("00" + this.state.counter, "hoodle doodle", this.state.comment));
+      this.setState((state) => {
+        return {counter : state.counter + 1, comment: ''};
+          }
+    );
   }
 
   handleInput(event) {
@@ -84,7 +68,7 @@ class TimelineWithButton extends React.Component {
       <h>{this.state.counter}</h>
       <form onSubmit={this.handleClick}>
         <label>
-          Name:
+          Event:
           <input type="text" name="name" value={this.state.comment} onChange={event => this.handleInput(event)} />
         </label>
         <input type="submit" value="Submit" />
