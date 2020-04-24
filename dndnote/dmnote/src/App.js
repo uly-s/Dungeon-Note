1921a350963c602d643f15fa4d91c9fdfa7e857f
ulysses-io <grant.swalwell-1@ou.edu>
2020-04-24 13:00:05 -0500

    Checkpoint reached, almost there. I'm starting incremental saving to show what was done when

--- dndnote/dmnote/src/App.js
+++ dndnote/dmnote/src/App.js
@@ -2,48 +2,69 @@ import React from 'react';
 import './App.css';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import './react-tabs.css';
-import  Timeline  from './Timeline.js';
-import Map from "./MapPanel.js";
+import  Timeline  from './components/Timeline.js';
+import Map from "./components/MapPanel.js";
+import TablePanel from "./components/TablePanel.js";
+import Tracker from "./components/Tracker.js";
+
 //import Timeline from "./Timeline";
 //import AddTLI from "./Timeline";
 
+class App extends React.Component {
+  constructor(props)
+  {
+    super(props);
 
+    this.state = { data: [], firstName: "", lastName: "" };
+  }
 
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        <Tabs>
+          <TabList>
+            <Tab>Map</Tab>
+            <Tab>Timeline</Tab>
+            <Tab>Tables</Tab>
+            <Tab>Initiative</Tab>
+          </TabList>
+          
+          <TabPanel>
+            <div>
+              <Map />
+            </div> 
+          </TabPanel>
 
+          <TabPanel>
+            <div>
+              <Timeline/>
+            </div>
+          </TabPanel>
 
+          <TabPanel>
+            <div>
+              <TablePanel/>
+            </div>
+          </TabPanel>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <Tabs>
-        <TabList>
-          <Tab>Title 1</Tab>
-          <Tab>Title 2</Tab>
-        </TabList>
+          <TabPanel>
+            <div>
+              <Tracker/>
+            </div>
+          </TabPanel>
+          
+          
         
-        <TabPanel>
-          <div>
-            <Map />
-          </div> 
-        </TabPanel>
+        </Tabs>
+        </header>
 
-        <TabPanel>
-          <div>
-            <Timeline/>
-          </div>
-        </TabPanel>
+        <body className="App-body">
         
-        
-      
-      </Tabs>
-      </header>
-
-      <body className="App-body">
-      
-      </body>
-    </div>
-  );
+        </body>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ dndnote/dmnote/src/components/Tracker.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import './Tracker.css';
+import Example from './ButtonPanel.js';
+import { Button } from 'reactstrap';
+import TextArea from './TextWindow.js';
+import Table from './Table.js';
+
+var rolls = ['booga booga boo\n'];
+
+var columns = ["Name", "AC", "HP", "Init"];
+var entities = [{Name:"", AC:"", HP:"", Init:""}];
+
+
+class Tracker extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { numRolls : 0, roll:'', rows:entities};
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({numRolls:rolls.push(this.state.roll)});
+  }
+
+  handleInput(event) {
+    event.preventDefault();
+    this.setState({roll : event.target.value});
+  }
+
+  render() {
+    return (
+      <body className="App-body">
+        <div class="row">
+          <div class="col-1">
+            <Table header={columns} data={entities}/>            
+            </div> 
+          <div class="col-2">
+          
+            
+          </div> 
+
+          <div class="col-3">
+            <div>
+                <Button>d4</Button>
+                <Button>d6</Button>
+              </div>
+              <div>
+                <Button>d10</Button>
+                <Button>d12</Button>
+              </div>
+              <div>
+                <Button>d20</Button>
+                <Button>d100</Button>
+              </div>  
+              
+              <form onSubmit={this.handleSubmit}>
+                <input type="text" name="name" value={this.state.comment} onChange={event => this.handleInput(event)} />
+                <input type="submit" value="Submit" />
+              </form>
+          </div> 
+          <div class="col-4">  
+           <textarea readOnly="true" rows="40" cols="50" value={rolls}></textarea>
+          </div> 
+        </div>
+      </body>
+      
+    )
+  }
+}
+
+export default Tracker;
